0721a5ea58dd2b89d012e8bd7367948a936a6e32
Don Petersen <don@donpetersen.net>
2014-01-08 17:47:43 +0000

    Don't use EmailVeracity's domain checks.
    It's not what the client-side validation would do.  If we really want to
    bring this back for some reason, definitely shut it off in the
    spec_helper because it has a wee bit of an effect on the test times.

--- lib/formulary.rb
+++ lib/formulary.rb
@@ -2,5 +2,9 @@ require 'nokogiri'
 require 'active_support/core_ext/object/blank'
 require 'email_veracity'
 
+# Don't try and determine if this email address is legitimate, just validate
+# the provided address.
+EmailVeracity::Config[:skip_lookup] = true
+
 require "formulary/version"
 require "formulary/html_form"
